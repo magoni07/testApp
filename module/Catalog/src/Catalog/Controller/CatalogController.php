4359279bec1768968798407206263cb6c1f0559f
magoni <magoni0786@gmail.com>
2016-05-05 10:35:19 +0300

    create Project

---
+++ module/Catalog/src/Catalog/Controller/CatalogController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Catalog\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class CatalogController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function detailAction()
+    {
+    }
+}
